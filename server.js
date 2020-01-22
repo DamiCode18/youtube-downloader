bf2c0bd013afe436f8ba36a2cce220026305a649
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2020-01-22 16:18:13 +0100

    new commit

--- server.js
+++ server.js
@@ -3,13 +3,22 @@ const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
 app.use(cors());
-app.listen(5000, () => {
-	console.log('Server Works !!! At port 5000');
-});
+
 app.get('/download', (req, res) => {
 	var URL = req.query.URL;
 	res.header('Content-Disposition', 'attachment; filename="video.mp4"');
 	ytdl(URL, {
 		format : 'mp4'
 	}).pipe(res);
+});
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+app.listen(5000, () => {
+	console.log('Server Works !!! At port 5000');
 });
