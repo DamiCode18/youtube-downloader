8b4933abe8a9c777ab89a8309e3420f9f1a94adc
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2020-02-08 09:45:25 +0100

    empty input not triggering download

--- client/src/components/Ydownloader.js
+++ client/src/components/Ydownloader.js
@@ -1,32 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Ydownloader.css';
-function onSubmit(e) {
-	e.preventDefault();
-}
-function download(e) {
-	var btn = document.getElementById('convert-button');
-	var URLinput = document.getElementById('URL-input');
 
-	sendURL(URLinput.value);
-}
 function sendURL(URL) {
-	window.location.href = `http://localhost:5000/download?URL=${URL}`;
+	if (!URL) {
+		window.location.href = '/';
+	} else {
+		window.location.href = `http://localhost:5000/download?URL=${URL}`;
+	}
 }
+let btn, URLinput;
+class Ydownloader extends Component {
+	onSubmit = (e) => {
+		e.preventDefault();
+	};
+	download = (e) => {
+		e.preventDefault();
+		btn = document.getElementById('convert-button');
+		URLinput = document.getElementById('URL-input');
 
-const Ydownloader = () => {
-	return (
-		<div className='y-down'>
-			<h1 className='heading'>
-				<span style={{color: 'red', margin: '5px'}}>YouTube</span>
-				<span style={{color: 'green', margin: '5px'}}>Downloader!</span>
-			</h1>
-			<form onSubmit={onSubmit}>
-				<input type='text' id='URL-input' placeholder='paste your link!' />
-				<div>
-					<input type='button' id='convert-button' className='btn' onClick={download} value='Download Now' />
-				</div>
-			</form>
-		</div>
-	);
-};
+		sendURL(URLinput.value);
+	};
+
+	render() {
+		return (
+			<div className='y-down'>
+				<h1 className='heading'>
+					<span style={{color: 'red', margin: '5px'}}>YouTube</span>
+					<span style={{color: 'green', margin: '5px'}}>Downloader!</span>
+				</h1>
+				<form onSubmit={this.onSubmit}>
+					<input type='text' id='URL-input' placeholder='paste your link!' />
+					<div>
+						<input
+							type='button'
+							id='convert-button'
+							className='btn'
+							onClick={this.download}
+							value='Download Now'
+						/>
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
 export default Ydownloader;
